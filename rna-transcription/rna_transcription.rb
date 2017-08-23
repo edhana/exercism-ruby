9043a48b7bd335581cd2018a8269c3d3ae6269ae
Eduardo Marques <edhana@gmail.com>
2017-08-23 15:07:31 -0300

    Removed some unecessary validations from the rna_transcription exercism

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,7 +1,7 @@
 class Complement
   def Complement.of_dna strand
     parser = {"G" => "C", "C" => "G", "T" => "A", "A" => "U"}
-    return strand.gsub(/[GCTA]/, parser) if strand && (strand.chars.all? { |c| "CGTA".chars.include? c })
+    return strand.gsub(/[GCTA]/, parser) if (strand.chars.all? { |c| "CGTA".chars.include? c })
 
     ''
   end
